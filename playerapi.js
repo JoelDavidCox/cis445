5aa3d9146d1119e7787035dc563fd7493ac00e58
Joel Cox <jcox6@una.edu>
2019-02-19 23:43:06 +0000

    restful api project

--- playerapi.js
+++ playerapi.js
@@ -3,29 +3,16 @@ const app = express();
 
 app.get('/', function(request,response) {
     response.writeHead(200);
-    response.write('<html><body><p>Please specify an player in the URL path</p></body></html>');
+    response.write('<html><body><p>Please specify a player, item, enemy, NPC, tree, or texture in the URL path.</p></body></html>')
 });
 
-
-app.listen(8080, function() {
-    console.log('express is now running on port 8080...');
-    
-});
-
-/*app.put('/:player', function(request, response) {
-    const player = request.params.player;
-    response.writeHead(200);
-    response.write("<html><body><p>" +player+ " does not have the attribute you are looking for.</p></body></html>");
-    response.end();
-});
-*/
 //get players
 app.get('/players', function(request, response) {
     response.json({player1: 'Joe', player2: 'George'});
     response.end();
 });
 //get player
-app.get('/:player', function(request, response) {
+app.get('/players/:player', function(request, response) {
     const player = request.params.player;
     response.json("Hello, you have received the " +player);
     response.end();
@@ -42,10 +29,10 @@ app.post('/players/:player', function(request, response) {
     response.json("Hello, you have created the " +player);
     response.end();
 });
-//update player
-app.patch('/players/:player', function(request, response) {
+//replace player
+app.put('/players/:player', function(request, response) {
     const player = request.params.player;
-    response.json("Hello, you have updated the " +player);
+    response.json("Hello, you have replaced the " +player);
     response.end();
 });
 
@@ -56,7 +43,7 @@ app.get('/items', function(request, response) {
 });
 
 //get item
-app.get('/:item', function(request, response) {
+app.get('/items/:item', function(request, response) {
     const item = request.params.item;
     response.json("Hello, you have recieved the " +item);
     response.end();
@@ -76,9 +63,150 @@ app.post('/items/:item', function(request, response) {
     response.end();
 });
 
-//update item
-app.patch('/items/:item', function(request, response) {
+//replace item
+app.put('/items/:item', function(request, response) {
     const item = request.params.item;
-    response.json("Hello you have updated the " +item);
+    response.json("Hello, you have replaced the " +item);
     response.end();
+});
+
+//get enemies
+app.get('/enemies', function(request, response) {
+    response.json({enemy1: 'hive', enemy2: 'fallen'});
+    response.end();
+});
+
+//get enemy
+app.get('/enemies/:enemy', function(request, response) {
+    const enemy = request.params.enemy;
+    response.json("Hello, you have recieved the " +enemy);
+    response.end();
+});
+
+//delete enemy
+app.delete('/enemies/:enemy', function(request, response) {
+    const enemy = request.params.enemy;
+    response.json("Hello, you have terminated the " +enemy);
+    response.end();
+});
+
+//create enemy
+app.post('/enemies/:enemy', function(request, response) {
+    const enemy = request.params.enemy;
+    response.json("Hello, you have created the " +enemy);
+    response.end();
+});
+
+//replace enemy
+app.put('/enemies/:enemy', function(request, response) {
+    const enemy = request.params.enemy;
+    response.json("Hello, you have replaced the " +enemy);
+    response.end();
+});
+
+
+//get Non Playable Characters
+app.get('/NPCs', function(request, response) {
+    response.json({NPC1: 'Zora', NPC2: 'Goron'});
+    response.end();
+});
+
+//get NPC
+app.get('/NPCs/:NPC', function(request, response) {
+    const NPC = request.params.NPC;
+    response.json("Hello, you have recieved the " +NPC);
+    response.end();
+});
+
+//delete NPC
+app.delete('/NPCs/:NPC', function(request, response) {
+    const NPC = request.params.NPC;
+    response.json("Hello, you have terminated the " +NPC);
+    response.end();
+});
+
+//create NPC
+app.post('/NPCs/:NPC', function(request, response) {
+    const NPC = request.params.NPC;
+    response.json("Hello, you have created the " +NPC);
+    response.end();
+});
+
+//replace NPC
+app.put('/NPCs/:NPC', function(request, response) {
+    const NPC = request.params.NPC;
+    response.json("Hello, you have replaced the " +NPC);
+    response.end();
+});
+
+//get trees
+app.get('/trees', function(request, response) {
+    response.json({tree1: 'birch', item2: 'oak'});
+    response.end();
+});
+
+//get tree
+app.get('/trees/:tree', function(request, response) {
+    const tree = request.params.tree;
+    response.json("Hello, you have recieved the " +tree);
+    response.end();
+});
+
+//delete tree
+app.delete('/trees/:tree', function(request, response) {
+    const tree = request.params.tree;
+    response.json("Hello, you have terminated the " +tree);
+    response.end();
+});
+
+//create tree
+app.post('/trees/:tree', function(request, response) {
+    const tree = request.params.tree;
+    response.json("Hello, you have created the " +tree);
+    response.end();
+});
+
+//replace tree
+app.put('/trees/:tree', function(request, response) {
+    const tree = request.params.tree;
+    response.json("Hello, you have replaced the " +tree);
+    response.end();
+});
+
+//get textures
+app.get('/textures', function(request, response) {
+    response.json({item1: 'grass', item2: 'bark'});
+    response.end();
+});
+
+//get texture
+app.get('/textures/:texture', function(request, response) {
+    const texture = request.params.texture;
+    response.json("Hello, you have recieved the " +texture);
+    response.end();
+});
+
+//delete texture
+app.delete('/textures/:texture', function(request, response) {
+    const texture = request.params.texture;
+    response.json("Hello, you have terminated the " +texture);
+    response.end();
+});
+
+//create texture
+app.post('/textures/:texture', function(request, response) {
+    const texture = request.params.texture;
+    response.json("Hello, you have created the " +texture);
+    response.end();
+});
+
+//replace texture
+app.put('/textures/:texture', function(request, response) {
+    const texture = request.params.texture;
+    response.json("Hello, you have replaced the " +texture);
+    response.end();
+});
+
+app.listen(8080, function() {
+    console.log('express is now running on port 8080...');
 });
