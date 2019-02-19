05ad2667f4ba9f98021615a586533cde9dc0c47a
Joel Cox <jcox6@una.edu>
2019-02-19 18:51:29 +0000

    restful api project

--- playerapi.js
+++ playerapi.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', function(request,response) {
     response.writeHead(200);
-    response.write('<html><body><p>Please specify an player in the URL path</p></body></html>')
+    response.write('<html><body><p>Please specify an player in the URL path</p></body></html>');
 });
 
 
@@ -19,75 +19,66 @@ app.listen(8080, function() {
     response.end();
 });
 */
-
+//get players
+app.get('/players', function(request, response) {
+    response.json({player1: 'Joe', player2: 'George'});
+    response.end();
+});
 //get player
 app.get('/:player', function(request, response) {
     const player = request.params.player;
-    response.writeHead(200);
-    response.write("<html><body><p>Hello, some" +player+ " is playing.</p>" + 
-                    "<p>I see you are calling: " +player+ "</p>");
-    response.write("<p>" +player+ " is really bad at video games.</p></body></html>");
+    response.json("Hello, you have received the " +player);
     response.end();
 });
 //delete player
-app.delete('/:player', function(request, response){
+app.delete('/players/:player', function(request, response){
     const player = request.params.player;
-    response.writeHead(200);
-    response.write("<html><body><p>Hello, you have terminated the " +player);
-    response.write("</p></body></html>");
+    response.json("Hello, you have terminated the " +player);
     response.end();
 });
 //create player
-app.post('/:player', function(request, response) {
+app.post('/players/:player', function(request, response) {
     const player = request.params.player;
-    response.writeHead(200);
-    response.write("<html><body><p>" +player+ " has been created.</p></body></html>");
+    response.json("Hello, you have created the " +player);
     response.end();
 });
 //update player
-app.patch('/:player', function(request, response) {
+app.patch('/players/:player', function(request, response) {
     const player = request.params.player;
-    response.writeHead(200);
-    response.write("<html><body><p>" +player+ " has been updated.</p></body></html>");
+    response.json("Hello, you have updated the " +player);
     response.end();
 });
 
 //get items
-app.get('/:items', function(request, response) {
-    const items = request.params.player;
-    response.writeHead(200);
-    response.write("<html><body><p>" +items+ " has been retrieved.</p></body></html>");
+app.get('/items', function(request, response) {
+    response.json({item1: 'sword', item2: 'axe'});
     response.end();
 });
 
 //get item
 app.get('/:item', function(request, response) {
-    const item = request.params.player;
-    response.writeHead(200);
-    response.write("<html><body><p>" +item+ " has been retrieved.</p></body></html>");
+    const item = request.params.item;
+    response.json("Hello, you have recieved the " +item);
     response.end();
 });
 
 //delete item
-app.delete('/:item', function(request, response) {
-    const item = request.params.player;
-    response.writeHead(200);
-    response.write("<html><body><p>" +item+ " has been terminated.</p></body></html>");
+app.delete('/items/:item', function(request, response) {
+    const item = request.params.item;
+    response.json("Hello, you have terminated the " +item);
     response.end();
 });
 
 //create item
-app.post('/:item', function(request, response) {
-    const item = request.params.player;
-    response.writeHead(200);
-    response.write("<html><body><p>" +item+ " has been created.</p></body></html>");
+app.post('/items/:item', function(request, response) {
+    const item = request.params.item;
+    response.json("Hello, you have created the " +item);
     response.end();
 });
 
 //update item
-app.post('/:item', function(request, response) {
-    const item = request.params.player;
-    response.writeHead(200);
-    response.write("<html><body><p>" +item+ " has been updated.</p></body></html>");
+app.patch('/items/:item', function(request, response) {
+    const item = request.params.item;
+    response.json("Hello you have updated the " +item);
     response.end();
 });
