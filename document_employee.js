2f49d4b3b33a6e31226fa5c314d4c85ccb885f51
Joel Cox <jcox6@una.edu>
2019-02-20 00:58:55 +0000

    document employee homework

---
+++ document_employee.js
@@ -0,0 +1,39 @@
+     {   
+         {
+            "Employee_id": "<ObjectId>",
+            "employees": [
+               {
+                "emp_no": 1234567
+                "birth_date": "12/12/1977"
+                "first_name": "John"
+                "last_name": "Doe"
+                "salaries": [{"emp_no": 1234567}, {"salary":40000}, {"from date":"12/12/1999"}, {"to date": "12/12/2019"}]
+                "titles": [{"emp_no.": 1234567}, {"title": "tech"}, {"from_date": "12/12/1999"} {"to_date": "12/12/2019"}],
+                "dept_emp": [{"emp_no": 1234567}, {"dept_no": 3000}, {"from_date": "12/12/1999"}, {"to_date": "12/12/2019"}]
+               }
+          ]
+          
+            "department_id": "<ObjectId>"
+            "departments": [
+                {
+                    "dept_no": 3000
+                    "dept_name": "IT"
+                    "dept_emp": [{"emp_no": 1234567}, {"dept_no": 3000}, {"from_date": "12/12/1999"}, {"to_date": "12/12/2019"}]
+               
+                ]
+            
+                }
+                
+            "dept_manager_id": "<ObjectId>"
+            "dept_manager": [
+                {
+                    "dept_no": 3000
+                    "emp_no": 1234567
+                    "from_date": "12/12/1999"
+                    "to_date": "12/12/2019'
+                }
+                ]
+                
+         }
+     }
+    
